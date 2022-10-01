5628fb3fa5ed103ecbde4a25733daac1b75358da
Thaddeus <thaddeusleezx@gmail.com>
2022-10-01 17:12:35 +0800

    temporary login check

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="vue-template">
-        <form>
+        <form v-on:submit.prevent="login()">
             <h3>Sign In</h3>
  
             <div class="form-group">
                 <label>Email address</label>
-                <input type="email" class="form-control form-control-lg" />
+                <input type="email" class="form-control form-control-lg" v-model="form.email"/>
             </div>
  
             <div class="form-group">
                 <label>Password</label>
-                <input type="password" class="form-control form-control-lg" />
+                <input type="password" class="form-control form-control-lg" v-model="form.password"/>
             </div>
  
             <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
@@ -26,13 +26,45 @@
             </div>
  
         </form>
+        <h6>{{message}}</h6>
     </div>
 </template>
  
 <script>
+    import axios from 'axios';
     export default {
+        name: "Login",
         data() {
-            return {}
+            return {
+                form: {
+                    email: "",
+                    password: "",
+                },
+                message: "asd "
+            }
+        },
+        methods: {
+            login () {
+                axios.post('https://us-central1-wad2-eventhive-backend-d0f2c.cloudfunctions.net/app/api/users/signin', 
+                    {
+                        userEmail: this.form.email,
+                        userPassword: this.form.password,
+                    }
+                )
+                .then(function(response) {
+
+                    if (response.data == "Register") {
+                        this.message = "User not found, please register!"
+                    } 
+                    else if (response.data == "Incorrect password") {
+                        this.message = "Incorrect password, please check again!"
+                    } else {
+                        this.message = "Login successful"
+                    }
+
+                })
+
+            }
         }
     }
 </script>
