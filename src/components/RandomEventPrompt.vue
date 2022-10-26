1d26725072db0994e59e55fde6ee2d0166f34719
TAN JIA MIN, JASMINE _ <jasmine.tan.2019@sis.smu.edu.sg>
2022-10-27 02:39:39 +0800

    create floating action button

---
+++ src/components/RandomEventPrompt.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn @click="toRandomEvent" elevation="4" class="greenLight ma-5 d-none d-sm-flex" icon height="120px" width="120px" absolute bottom right>
+    <v-img transition="scale-transition" 
+      src="../src/assets/images/beebutton.png"
+      height="100px" contain></v-img>
+  </v-btn>
+</template>
+<script>
+
+export default {
+  name: 'RandomEventPrompt',
+  methods: {
+    toRandomEvent() {
+      this.$router.push("/events/random");
+    }
+  }
+}
+</script>
+
