bb3732aeb0dc4dc738c7cffa4ceb1c0ec3134154
TAN JIA MIN, JASMINE _ <jasmine.tan.2019@sis.smu.edu.sg>
2022-10-30 15:27:16 +0800

    bugfix

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -47,6 +47,7 @@
               v-bind="attrs"
               v-on="on"
               color="greenDark"
+              @focus="isAdvanced = true"
             ></v-text-field>
           </template>
           <v-date-picker
