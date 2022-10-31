9745fda056499d805106d188181574bda81b210d
developerlxy <esther.lam.2020@business.smu.edu.sg>
2022-10-31 15:48:36 +0800

    home view

--- src/components/RandomEventPrompt.vue
+++ src/components/RandomEventPrompt.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-btn @click="toRandomEvent" elevation="4" class="greenLight ma-5 d-none d-sm-flex" icon height="120px" width="120px" absolute bottom right>
+  <v-btn @click="toRandomEvent" elevation="4" class="greenLight ma-5 d-none d-sm-flex" icon height="100px" width="100px" absolute bottom right>
     <v-img transition="scale-transition" 
       src="../src/assets/images/beebutton.png"
-      height="100px" contain></v-img>
+      height="80px" contain></v-img>
   </v-btn>
 </template>
 <script>
