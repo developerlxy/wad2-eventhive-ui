e15386c6640657aaf38940b5a048a1fbf44af48a
Thaddeus <thaddeusleezx@gmail.com>
2022-11-12 20:36:01 +0800

    host profile mobile friendly

--- src/views/HostProfile.vue
+++ src/views/HostProfile.vue
@@ -4,50 +4,133 @@
 
   <div v-else>
       <NavBar></NavBar>
+
+    <div v-if="xsBreakpoint">
       <v-container fluid class="peachLight">
+        <v-row
+          no-gutters
+          style="flex-wrap: nowrap;"
+          class="mx-4"
+        >
+          <v-col
+            cols="12"
+            class="flex-grow-0 flex-shrink-0"
+          >
+            <v-card
+              class="pt-4"
+              outlined
+              tile
+            >
+            <v-avatar size="150" class="mt-2">
+                <img :src="'/src/assets/images/test.png'">
+            </v-avatar>
+            <v-rating
+              :value="avgRating"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+              ></v-rating>
+            
+            <v-card
+              class="mx-auto py-4"
+              outlined
+            >
+        <v-list-item three-line >
+          <v-list-item-content>
+            <div style="background-color:lightgray;padding:10px;text-align:left;font-weight:bold;">
+              Contact Info
+            </div>
+            <v-list-item style="text-align:left" class=""><br>
+              Email: {{ host["userEmail"] }} <br><br>
+              Contact No.: {{ host['userPhone'] }}
+            </v-list-item>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+      
+
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        outlined
+      >
+    
+      </v-card>
+            </v-card>
+          
+          </v-col>
+        </v-row>
+          <v-col
+            cols="2"
+            style="min-width: 100px; max-width: 100%;"
+            class="flex-grow-1 flex-shrink-0"
+          >
+            <v-card
+              class="pa-2 text-left px-8 pt-4"
+              outlined
+              tile
+            >
+              <h1>Hey, I'm {{ host["userName"] }}!</h1><br/>
+              <p>Descripion here...</p>
+              <h2>Rating & Reviews</h2><br>
+              <div v-if="reviews.length==0">
+              <p>No reviews yet...</p>
+            </div>
+              <div v-else>
+              <HostEventReview  v-for="review of reviews" :eventReview="review"></HostEventReview>
+              </div>
+            </v-card>
+          </v-col>
+      </v-container>
+    </div>
+    <div v-else>
+      <v-container fluid class="peachLight pb-16">
     <v-row
       no-gutters
       style="flex-wrap: nowrap;"
+      class="mx-4 mb-16"
     >
       <v-col
-        cols="2"
+        cols="3"
         class="flex-grow-0 flex-shrink-0"
       >
         <v-card
-          class="pa-2"
+          class="pt-2 mr-4"
           outlined
           tile
         >
-        <v-avatar size="150">
+        <v-avatar size="150" class="mt-2">
             <img :src="'/src/assets/images/test.png'">
         </v-avatar>
         <v-rating
-                                :value="avgRating"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="14"
-                                ></v-rating>
+          :value="avgRating"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+          ></v-rating>
         
         <v-card
-    class="mx-auto"
-    max-width="344"
-    outlined
-  >
+          class="mx-auto pb-4"
+          outlined
+        >
+
     <v-list-item three-line>
       <v-list-item-content>
         <div style="background-color:lightgray;padding:10px;text-align:left;font-weight:bold;">
           Contact Info
         </div>
-        <v-list-item style="text-align:left"><br>
-          Email: {{ host["userEmail"] }} <br><br>
-          Contact No.: {{ avgRating }}
-        </v-list-item>
-
+         <v-list-item style="text-align:left;word-wrap: break-word;" class="px-2"><br>
+          Email: {{ host["userEmail"] }} 
+          <br><br>
+          Contact No.: {{ host['userPhone'] }}
+      </v-list-item>
       </v-list-item-content>
-
     </v-list-item>
+   
   </v-card>
   <v-card
     class="mx-auto"
@@ -64,7 +147,7 @@
         class="flex-grow-1 flex-shrink-0"
       >
         <v-card
-          class="pa-2 text-left"
+          class="pa-2 text-left px-8 pt-4"
           outlined
           tile
         >
@@ -81,6 +164,8 @@
       </v-col>
     </v-row>
   </v-container>
+    </div>
+      
       
     </div>
   </div>
@@ -124,9 +209,12 @@
             if (event["eventHost"]!=null && event["eventHost"]["_id"]==this.host["_id"]) {
               returnArray=returnArray.concat(event["eventReviews"]) 
             }
-        }
+          }
           return returnArray
-      },
+        },
+        xsBreakpoint() {
+          return this.$vuetify.breakpoint.name == 'xs' 
+        },
       avgRating() {
         let count = this.reviews.length
         let total = 0
